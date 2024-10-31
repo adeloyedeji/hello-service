4cb449b0081259c61328bf76a9d9971b22bf76f6
Adedeji Adeloye <adeloyedeji@gmail.com>
2024-10-31 17:01:11 +0100

    Little bootstrapping

--- src/main/java/com/example/hello/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/hello/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/hello")
 public class HelloController {
     @GetMapping
-    public String getMethodName() {
+    public String helloString() {
         return "Hello World";
     }
     
